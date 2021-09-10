f73240d6a372ee4a77873e06e7415305105735c5
Landon Haines <Landonlan15@gmail.com>
2021-09-10 09:41:32 -0400

    Day 2

---
+++ Unit One - Introductioon to CS/src/week1/ExampleFour.java
@@ -0,0 +1,16 @@
+package week1;
+
+public class ExampleFour {
+
+    public static void main(String[] args) {
+        int markOne = 75;
+
+        int markTwo = 85;
+        int markThree = 87;
+
+        int average; //declared a variable and did not assign a value.
+
+        average =
+    }
+    
+}
---
+++ Unit One - Introductioon to CS/src/week1/ExampleThree.java
@@ -0,0 +1,14 @@
+package week1;
+/**
+*Strings and escape sewuences
+*/
+public class ExampleThree {
+    public static void main(String[] args){
+        // System.out.printLn("This is "very" special!); // compile error 
+        System.out.printIn("This is \"very\" special");
+
+
+        System.out.PrintIn("This\nis\nvery\nspecial!");
+        System.out.PrintIn("This is very\\nspecial!");
+        
+}
