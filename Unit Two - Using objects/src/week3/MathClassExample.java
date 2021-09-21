c7747a24a6da436bedb191d2281f85cf2ad5f026
Landon Haines <Landonlan15@gmail.com>
2021-09-21 15:30:53 -0400

    Test commit

---
+++ Unit Two - Using objects/src/week3/MathClassExample.java
@@ -0,0 +1,13 @@
+package week3;
+
+public class MathClassExample {
+    
+    public static void main(String[] args){
+        double a = -7.8;
+    
+        System.out.println(Math.abs(a));
+        System.out.println(Math.sqrt(10));
+    }
+}
+
+
---
+++ Unit Two - Using objects/src/week3/WrapperclassExample.java
@@ -0,0 +1,18 @@
+package week3;
+
+public class WrapperclassExample {
+    public static void main(String[] args) {
+
+    Integer n = new Integer (7);
+    double d = new Double(3.2);
+
+    Integer num = 7;
+    int m = num;
+
+    int x = 6 + num;
+    int y = m + num;
+    
+    int z = num.intValue();
+
+    }
+}
